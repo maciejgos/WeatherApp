1dad37b523b40c6de8310703321e4e17879b743f
Maciej Gos <maciej@maciejgos.com>
2018-09-22 17:47:24 +0200

    Custom Navigation Page

---
+++ WeatherApp/Application/Navigation/Controls/WeatherAppNavigationPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WeatherApp.Application.Navigation.Controls
+{
+    public class WeatherAppNavigationPage : NavigationPage
+    {
+        public WeatherAppNavigationPage()
+        {
+        }
+    }
+}
