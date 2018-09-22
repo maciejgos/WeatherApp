3dd765b25161ff74c7752e5b506aa1f720f2ccfe
Maciej Gos <maciej@maciejgos.com>
2018-09-22 17:48:19 +0200

    Create LocalWeatherPage

---
+++ WeatherApp/Features/Local/Views/LocalWeatherPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WeatherApp.Features.Local.Views
+{
+    public partial class LocalWeatherPage : ContentPage
+    {
+        public LocalWeatherPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
