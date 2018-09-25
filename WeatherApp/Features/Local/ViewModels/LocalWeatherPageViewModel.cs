5e98309d4bba48fe8da0df70d34ab01c6ec22c2f
Maciej Gos <maciej@maciejgos.com>
2018-09-25 19:46:26 +0200

    Properties implementation

--- WeatherApp/Features/Local/ViewModels/LocalWeatherPageViewModel.cs
+++ WeatherApp/Features/Local/ViewModels/LocalWeatherPageViewModel.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace WeatherApp.Features.Local.ViewModels
 {
     public class LocalWeatherPageViewModel : BindableBase
     {
+        string _city = "Warszawa";
+        string _temp = "7";
+
+        public string City { get => _city; set => SetProperty(ref _city, value); }
+        public string Temp { get => _temp; set => SetProperty(ref _temp, value); }
+
+        public DelegateCommand GetCurrentWeatherCommand { get; set; }
+
         public LocalWeatherPageViewModel()
         {
+            GetCurrentWeatherCommand = new DelegateCommand(async () => await OnGetCurrentWeatherCommand());
+        }
+
+        async Task OnGetCurrentWeatherCommand()
+        {
+            throw new NotImplementedException();
         }
     }
 }
