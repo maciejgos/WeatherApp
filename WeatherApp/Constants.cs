4ca854c62ef22edc6dce9a85b86df79066dfbfb2
Maciej Gos <maciej@maciejgos.com>
2018-09-26 21:12:52 +0200

    Rename README.md

--- WeatherApp/Constants.cs
+++ WeatherApp/Constants.cs
@@ -3,6 +3,6 @@
     public class Constants
     {
         public const string BaseUrl = "https://api.openweathermap.org";
-        public const string AppID = "{YOUR_API_KEY}";
+        public const string AppID = "0d039917e0d2820c0dc9ab4f95cf10d8";
     }
 }
