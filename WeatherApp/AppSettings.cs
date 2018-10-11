fa5a137b0f98648d4d466ddce56f37bc96abc161
Maciej Gos <maciej@maciejgos.com>
2018-10-11 11:17:23 +0200

    Add Mocks property & refactor

--- WeatherApp/AppSettings.cs
+++ WeatherApp/AppSettings.cs
@@ -4,16 +4,21 @@
     {
         // Open Weather Endpoints
         const string defaultEndpoint = "https://api.openweathermap.org";
-        const string apiKey = "{YOUR_API_KEY}";
+        const string defaultApiKey = "{YOUR_API_KEY}";
 
         // App Center
         const string defaultAppCenteriOS = "";
         const string defaultAppCenterAndroid = "";
 
+        // Fake
+        const bool defaultUseMocks = false;
+
         public static string WeatherEndpoint => defaultEndpoint;
-        public static string WeatherApiKey => apiKey;
+        public static string WeatherApiKey => defaultApiKey;
 
         public static string AppCenteriOSApiKey => defaultAppCenteriOS;
         public static string AppCenterAndroidApiKey => defaultAppCenterAndroid;
+
+        public static bool UseMocks => defaultUseMocks;
     }
 }
