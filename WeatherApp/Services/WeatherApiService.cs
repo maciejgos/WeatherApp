b8cbe9ca168cec5f05efe69a93bd1a0727f0698a
Maciej Gos <maciej@maciejgos.com>
2018-10-11 18:21:48 +0200

    Remove usings

--- WeatherApp/Services/WeatherApiService.cs
+++ WeatherApp/Services/WeatherApiService.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using WeatherApp.Models;
 
 namespace WeatherApp.Services
