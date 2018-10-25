86a8be4c1135ac7438522dd87e892fbd40b73e21
Maciej Gos <maciej@maciejgos.com>
2018-10-25 21:43:07 +0200

    Use of Refit GET method

--- WeatherApp/Services/IWeatherService.cs
+++ WeatherApp/Services/IWeatherService.cs
@@ -1,10 +1,12 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
+using Refit;
 
 namespace WeatherApp.Services
 {
     public interface IWeatherService
     {
-        Task<HttpResponseMessage> GetCurrentWeatherAsync(string city);
+        [Get("weather?q={city}&appid={appid}&units={units}")]
+        Task<HttpResponseMessage> GetCurrentWeatherAsync(string city, string appid = "000000", string units = "metric");
     }
 }
