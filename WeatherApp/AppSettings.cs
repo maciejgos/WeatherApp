e499cc3b007998b5957e55ade9d149922eaa78c2
Maciej Gos <maciej@maciejgos.com>
2018-11-10 23:52:53 +0100

    Define new properties in settings

--- WeatherApp/AppSettings.cs
+++ WeatherApp/AppSettings.cs
@@ -2,8 +2,13 @@ namespace WeatherApp
 {
     public class AppSettings
     {
-        const bool  _defaultIsInDesignMode = true;
+        private const bool  _defaultIsInDesignMode = true;
+        private const string _apiKey = "{YOUR_API_KEY}";
+        private const string _weatherApiEndpoint = "https://api.openweathermap.org/data/2.5/";
 
         public static bool IsInDesignMode { get; set; } = _defaultIsInDesignMode;
+
+        public static string ApiKey => _apiKey;
+        public static string WeatherApiEndpoint => _weatherApiEndpoint;
     }
 }
