75fc5113da945f088038eb53de902c29fbe3d3f3
Maciej Gos <maciej@maciejgos.com>
2018-11-10 23:48:39 +0100

    Create fields & NotifyPropertyChanged

--- WeatherApp/ViewModels/CurrentViewModel.cs
+++ WeatherApp/ViewModels/CurrentViewModel.cs
@@ -1,19 +1,27 @@
-using System.Collections;
 using System.Collections.Generic;
+using MvvmHelpers;
 
 namespace WeatherApp.ViewModels
 {
-    public class CurrentViewModel
+    public class CurrentViewModel : BaseViewModel
     {
-        public int Temperature { get; set; }
-        public string City { get; set; }
-        public string Unit { get; set; }
-        public IEnumerable<ForecastLineViewModel> WeeklyForecast { get; set; }
+        private int _temperature;
+        private string _city;
+        private string _unit;
+        private IEnumerable<ForecastLineViewModel> _weeklyForecast;
+
+        public int Temperature { get => _temperature; set => SetProperty(ref _temperature, value); }
+        public string City { get => _city; set => SetProperty(ref _city, value); }
+        public string Unit { get => _unit; set => SetProperty(ref _unit, value); }
+        public IEnumerable<ForecastLineViewModel> WeeklyForecast { get => _weeklyForecast; set => SetProperty(ref _weeklyForecast, value); }
     }
 
-    public class ForecastLineViewModel
+    public class ForecastLineViewModel : BaseViewModel
     {
-        public string Day { get; set; }
-        public int Temperature { get; set; }
+        private string _day;
+        private int _temperature;
+        
+        public string Day { get => _day; set => SetProperty(ref _day, value); }
+        public int Temperature { get => _temperature; set => SetProperty(ref _temperature, value); }
     }
 }
